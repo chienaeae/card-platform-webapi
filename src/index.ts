d93bdfbb1dc9243217ca893f93e7aa1d3f1c36ab
chienaeae <chienaeae@gmail.com>
2021-10-24 02:22:45 +0800

    gracefully shut down

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,55 @@ import {App} from "./infra/http/app";
 import * as dotenv from "dotenv";
 import cardPlatformSequel from "./infra/sequlize/config/config";
 import {orderingQueuePublisher} from "./infra/sqs/config/config";
-import {CardTradeRepo} from "./modules/cardOrdering/repos/CardTradeRepo";
 
 dotenv.config();
 
 export class Launcher {
-    private server: App;
+    private readonly server: App;
 
     public constructor() {
         this.server = new App();
     }
 
     public async launchApp() {
-        const preCheckedResult = await cardPlatformSequel.authConnection() && await orderingQueuePublisher.authQueuesStatus()
-
-        if(!preCheckedResult){
+        const preCheckedResult = await cardPlatformSequel.authConnection() &&
+            await orderingQueuePublisher.authQueuesStatus()
+        if (!preCheckedResult) {
             process.exit(1);
         }
-
         this.server.startServer();
     }
 
+    public async close() {
+        await this.server.closeServer();
+    }
 }
 
-new Launcher().launchApp();
-
-async function main(){
-
-    // const repo = new CardTradeRepo(cardPlatformSequel.models.cardTradeModel, cardPlatformSequel.models.cardOrderModel)
-    // const result = await repo.findLatestCardTradeByCardIndex(5, 2)
-    // console.log(result);
-
+async function main() {
+    const launcher = new Launcher();
+    await launcher.launchApp();
+    const sigs = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    sigs.forEach(sig => {
+        process.on(sig, async () => {
+            setTimeout(() => {
+                console.error(`Could not close connections in time, forcefully shutting down`);
+                process.exit();
+            }, 3000);
+            try {
+                console.info(`shutting down`);
+                await launcher.close()
+                await cardPlatformSequel.sequelInstance.close();
+                console.info(`shut down successfully`);
+                process.exit();
+            } catch (err) {
+                console.error(`closing with error occurred: ${err.message}`)
+            }
+        });
+    })
 }
-// main()
+
+main()
+
+
+
+
